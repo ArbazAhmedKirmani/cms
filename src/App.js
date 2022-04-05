2f9451fdf889c8403251ebc4d809e8a7ec6fac0b
Syed Arbaz Ahmed <syedarbaz.ahmed@sybrid.com>
2022-04-06 00:26:19 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Website from "./screens/website";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Website />
+      </div>
+    );
+  }
+}
+
+export default App;
